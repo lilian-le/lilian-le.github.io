d26715357c1a08779e1468aa6422f2c6ee7f54b7
lilian-le <33736999+lilian-le@users.noreply.github.com>
2017-11-16 15:41:05 -0800

    created math function

---
+++ main.js
@@ -0,0 +1,10 @@
+function math() {
+  var answer = prompt("Enter 1+1:");
+  if (answer == 2) {
+    confirm("You got it right. Your answer was " + answer);
+  }
+  else {
+    confirm("You got it wrong");
+  }
+}
+    
